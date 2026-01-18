8df1b430ca366b93bdc0961a878398df69671eec
daishori1 <wairu63@gmail.com>
2026-01-18 02:09:23 -0600

    feat('creation of the connection to the db , and add the diagram to how to work the aplication')

--- connection.py
+++ connection.py
@@ -1,2 +1,22 @@
-import os , psycopg2 
-from dotenv import load_dotenv
+import os , psycopg2  # import os and psy for psql 
+from dotenv import load_dotenv #for read the .env archive and dont expose de credentials
+
+load_dotenv() #load the .env credentials and initalize the server
+conn = None
+try :
+    conn = psycopg2.connect(   #read the credentials whit a alias for dont expose the real credentials
+    host = os.getenv("PGHOST"),
+    port = os.getenv("PGPORT"),
+    user = os.getenv("PGUSER"),
+    password = os.getenv("PGPASSWORD"),
+    dbname = os.getenv("PGDATABASE"), 
+    sslmode="require"
+)
+    print("connection status : properly connected")
+except psycopg2.OperationalError:
+    print("internal server error : code error 500")
+finally :
+    if conn is not  None:
+        conn.close
+
+
